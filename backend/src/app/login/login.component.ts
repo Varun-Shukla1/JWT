6b38c81bc584ef7f591519571da37b409bb6a8c0
Varun-Shukla1 <varunshukla2125@gmail.com>
2023-04-07 00:31:15 +0530

    Segrigation

---
+++ backend/src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { UserService } from '../servive/user.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private userService:UserService){
+
+  }
+
+  userLogin(data:any){
+
+  console.warn(data);
+  this.userService.login(data)
+  }
+}
